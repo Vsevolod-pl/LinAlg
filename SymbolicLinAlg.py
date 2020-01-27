f8074bad331d6cffe73e8ac94341167cc92be74c
Vsevolod-pl <vsevolod-pl@yandex.ru>
2020-01-27 22:12:37 +0300

    Removed useless code

--- SymbolicLinAlg.py
+++ SymbolicLinAlg.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.insert(0, ".")
-
 import LinAlgFuncs as la
 import mySybolicCalcs as sc
 
@@ -8,4 +5,4 @@ import mySybolicCalcs as sc
 def symbolic_tensor(var_name, *shape):
     if len(shape) == 1:
         return la.Tensor([sc.Variable(var_name+str(i)) for i in range(shape[0])])
-    return la.Tensor([symbolic_matrix(var_name+str(i), *shape[1:]) for i in range(shape[0])])
+    return la.Tensor([symbolic_tensor(var_name+str(i), *shape[1:]) for i in range(shape[0])])
